4aedcdfa916757515732d6b84b89dc41f767d6a4
Connor Westerman <connorwesterman@connors-mbp-10.myfiosgateway.com>
2024-02-07 23:45:45 -0500

    Adding the work for task 3.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,36 @@
 import React from "react";
 import "./App.css";
+import logo from "./Images/Logo.png";
+import { Button, Col, Container, Row } from "react-bootstrap";
 
 function App(): JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
                 UD CISC275 with React Hooks and TypeScript
+                <img src={logo} alt="UD's Logo" />
+                <h2>Connor Westerman</h2>
             </header>
-            <p>Connor Westerman</p>
+            <p> </p>
+            <Container>
+                <div className="App-column">
+                    <Row>
+                        <Col>Hello World</Col>
+                        <Col>Goodbye World</Col>
+                    </Row>
+                </div>
+            </Container>
+            <p> </p>
+            Colors:
+            <ol>
+                <li>Green</li>
+                <li>Blue</li>
+                <li>Red</li>
+            </ol>
+            <Button onClick={() => console.log("Hello World!")}>
+                Log Hello World
+            </Button>
+            <p> </p>
             <p>Hello World</p>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
